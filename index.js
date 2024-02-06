fc2f9a2852233c65a2c27e8e0d76f30e5fd831c5
La-Ruda <87708653+La-Ruda@users.noreply.github.com>
2024-02-06 19:15:16 +0100

    cc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("hello world");
+
+const heroInput = document.querySelector(".hero__input");
+const heroButton = document.querySelector(".hero__input__button");
+
+const heroSearchBar = document.querySelector(".hero__searchbar")
+
+
+const focusBlue = () => {
+    heroSearchBar.style.border = "2px solid skyblue";
+}
+
+heroInput.addEventListener("focus", focusBlue);
+
