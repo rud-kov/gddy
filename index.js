96de31ba28ba69550b00e6170a81179de6bd915f
La-Ruda <87708653+La-Ruda@users.noreply.github.com>
2024-02-07 20:10:58 +0100

    cc

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 console.log("hello world");
 
 const heroInput = document.querySelector(".hero__input");
-const heroButton = document.querySelector(".hero__input__button");
-
 const heroSearchBar = document.querySelector(".hero__searchbar")
 
-
 const focusBlue = () => {
-    heroSearchBar.style.border = "2px solid skyblue";
+    //heroSearchBar.style.border = "2px solid blue";
+    heroSearchBar.classList.add("hero__searchbar--focused")
+    console.log("lolo")
 }
 
+const unFocusBlue = () => {
+    heroSearchBar.classList.toggle("hero__searchbar--focused")
+}
+
+
 heroInput.addEventListener("focus", focusBlue);
+
+heroInput.addEventListener("blur", unFocusBlue)
 
