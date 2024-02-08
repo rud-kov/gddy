88bb7814cafe0955dbcc5dd8cf8ac4a8a47313ee
La-Ruda <87708653+La-Ruda@users.noreply.github.com>
2024-02-08 15:07:27 +0100

    commit

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ heroInput.addEventListener("focus", focusBlue);
 
 heroInput.addEventListener("blur", unFocusBlue)
 
+
+
+const sideScroller = document.querySelector(".reviews__box__wrapper");
+
+sideScroller.addEventListener("wheel", (event) => {
+    event.preventDefault();
+});
+
+
+const controlButtonLeft = document.querySelector(".review__control__button--left").addEventListener("click", (event) => {
+    sideScroller.scrollLeft -= 200;
+});
+
+const controlButtonRight = document.querySelector(".review__control__button--right").addEventListener("click", (event) => {
+    sideScroller.scrollLeft += 200;
+});
+
+
+
+
