d96663113a84684f5836acddd4d2ee3a2e2aa679
La-Ruda <87708653+La-Ruda@users.noreply.github.com>
2024-03-12 18:53:16 +0100

    otevirani/zavirani expandable menu bottombar Fixes #7

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const rollArrowMenu = () => {
 
 arrowDownSwitch.addEventListener("click", rollArrowMenu)
 
-
 /// HERO INPUT
 
 const input = document.querySelector(".input");
@@ -66,17 +65,9 @@ const observer = new IntersectionObserver(function(entries,observer) {
 
 observer.observe(reviewsWrapper);
 
-
-
 /// REVIEWS SIDESCROLLER
 
 const sideScroller = document.querySelector(".reviews__box__wrapper");
-
-// sideScroller.addEventListener("wheel", (event) => {
-//     event.preventDefault();
-// });
-
-
 
 const controlButtonLeft = document.querySelector(".review__control__button--left").addEventListener("click", (event) => {
     sideScroller.scrollLeft -= 200;
@@ -85,7 +76,6 @@ const controlButtonLeft = document.querySelector(".review__control__button--left
 const controlButtonRight = document.querySelector(".review__control__button--right").addEventListener("click", (event) => {
     sideScroller.scrollLeft += 200;
 });
-
 
 ////  FAQ ARROWS... 
 
@@ -99,60 +89,27 @@ faqDetails.forEach((faqDetail, index) => {
 });
 
 
-/************** FOOTER CURRENCY & LANGUAGUE   */
+/************** FOOTER CURRENCY & LANGUAGUE EXPANDMENUS  */
 
+function expandableMenuSwitch(event) {
 
-const footerLanguague = document.querySelector(".footer__bottombar__link--languague");
+    const clickedElement = event.target;
 
-const footerCurrency = document.querySelector(".footer__bottombar__link--currency");
+    expandableLinks.forEach((expandableLink) => {
 
-const expandmenuLanguage = document.querySelector(".footer__bottombar__expandmenu--language");
+        if (expandableLink.contains(clickedElement)) {
+            expandableLink.nextElementSibling.classList.toggle("open__menu");
+            expandableLink.children[1].classList.toggle("arrow--open");
+        } else {
+            expandableLink.nextElementSibling.classList.remove("open__menu");
+            expandableLink.children[1].classList.remove("arrow--open");
+        }
+    })
+}
 
-const expandmenuCurrency = document.querySelector(".footer__bottombar__expandmenu--currency");
+const expandableLinks = document.querySelectorAll(".expandable__link");
 
-const bottombarArrow = document.querySelector(".bottombar__arrow__up--languague");
-
-const bottombarArrowCurrency = document.querySelector(".bottombar__arrow__up--currency");
-
-footerLanguague.addEventListener("click", () => {
-
-    if (window.getComputedStyle(expandmenuLanguage).display === "none") {
-        expandmenuLanguage.classList.add("open__menu");
-        bottombarArrow.classList.add("arrow--open");
-    } else {
-        expandmenuLanguage.classList.toggle("open__menu");
-        bottombarArrow.classList.toggle("arrow--open")
-    };
-});
-
-
-footerCurrency.addEventListener("click", () => {
-    if (window.getComputedStyle(expandmenuCurrency).display === "none") {
-        expandmenuCurrency.classList.add("open__menu")
-        bottombarArrowCurrency.classList.add("arrow--open")
-    } else {
-        expandmenuCurrency.classList.toggle("open__menu")
-        bottombarArrowCurrency.classList.toggle("arrow--open")
-    }
-});
-
-
-//function toggleMenuOff(menuEl, arrowEl) {
-//    if (!menuEl.contains(event.target)) {
-//        menuEl.classList.remove("open__menu")
-//        arrowEl.classList.remove("arrow--open")
-//        //console.log("clicked elsewhere")
-//    } 
-//}
-//
-//document.addEventListener("click", function(event) {
-//    toggleMenuOff(expandmenuLanguage, bottombarArrow);
-//}) 
-//
-//document.addEventListener("click", function(event) {
-//    toggleMenuOff(expandmenuCurrency, bottombarArrowCurrency);
-//})
-
+document.addEventListener("click", expandableMenuSwitch);
 
 /******* CHANGING CURRENCY AND LANGUAGUE LINKS */
 
@@ -163,7 +120,6 @@ function expandMenuSwitch(buttons, control) {
         });
     });
 }
-
 
 const currencyButtons = document.querySelectorAll(".expandmenu__currency__button");
 const currencyControl = document.querySelector(".currency__switch");
